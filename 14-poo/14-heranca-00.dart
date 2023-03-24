d2626fba7713b1a00694b0fcde7958765089538e
Ricardo Taveira <ridtaveira@gmail.com>
2023-03-24 18:45:15 -0300

    Alterado em 24-03-2023

--- 14-poo/14-heranca-00.dart
+++ 14-poo/14-heranca-00.dart
@@ -14,7 +14,43 @@ class Cachorro extends Animal {
   Cachorro(String nome, int idade, bool estaVivo, this.raca)
       : super(nome, idade, estaVivo);
 
+  @override
   void verbalizar() {
     print('Au au!');
   }
+}
+
+class Gato extends Animal {
+  String raca;
+
+  Gato (String nome, int idade, bool estaVivo, this.raca)
+      : super(nome, idade, estaVivo);
+
+  @override
+  void verbalizar() {
+    print('MiiiAu Miiiau!');
+  }
+}
+
+class Pato extends Animal {
+  String raca;
+
+  Pato(String nome, int idade, bool estaVivo, this.raca)
+      : super(nome, idade, estaVivo);
+
+  @override
+  void verbalizar() {
+    print('Quack!');
+  }
+}
+
+// Utilizando as classes
+void main() {
+  Animal bicho1 = Cachorro("Rabito", 4, true, "Shitzu");
+  Animal bicho2 = Gato("Jerry", 3, true, "Mangará");
+  Animal bicho3 = Pato("Billy", 1, true, "Pé Duro");
+  
+  bicho1.verbalizar(); // Output: Au au!
+  bicho2.verbalizar(); // Output: Miau!
+  bicho3.verbalizar(); // Output: Quack!
 }
