e1fb3059c36e7bb1d965e6d0da147761ef3ea3c5
Ricardo Taveira <ridtaveira@gmail.com>
2023-03-26 22:32:06 -0300

    Alterado em 26-03-2023

---
+++ 14-poo/14-agregacao.dart
@@ -0,0 +1,31 @@
+// Agregação e Composição
+
+class Dependente {
+  late String _nome;
+
+  Dependente(String nome) {
+    this._nome = nome;
+  }
+}
+
+class Funcionario {
+  late String _nome;
+  late List<Dependente> _dependentes;
+
+  Funcionario(String nome, List<Dependente> dependentes) {
+    this._nome = nome;
+    this._dependentes = dependentes;
+  }
+}
+
+class EquipeProjeto {
+  late String _nomeProjeto;
+  late List<Funcionario> _funcionarios;
+
+  EquipeProjeto (String nomeprojeto, List<Funcionario> funcionarios) {
+    _nomeProjeto = nomeprojeto;
+    _funcionarios = funcionarios;
+
+  }
+
+}
