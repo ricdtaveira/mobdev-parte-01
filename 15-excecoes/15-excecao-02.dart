60961f1a99bcd57d54d2f30a1b37df4408b81096
Ricardo Taveira <ridtaveira@gmail.com>
2023-04-04 10:51:44 -0300

    Alterado em 04-04-2023

--- 15-excecoes/15-excecao-02.dart
+++ 15-excecoes/15-excecao-02.dart
@@ -1,18 +1,18 @@
-// 
+//
 //
 //
 
 import 'dart:io';
 
 void main() {
-  final file = File('meu_arquivo.txt');
+  final file = File('./meu_arquivo.txt');
   final conteudo = 'Conteúdo a ser gravado no arquivo';
 
   try {
     if (!file.existsSync()) {
       throw Exception('Arquivo não encontrado');
     }
-
+    // Grava no arquivo a string Conteudo
     file.writeAsStringSync(conteudo);
     print('Arquivo gravado com sucesso!');
   } on FileSystemException catch (e) {
