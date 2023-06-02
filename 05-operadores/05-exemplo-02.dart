c1cf3ec5e56f741ece11c6fe178315d2b8508582
Ricardo Taveira <ridtaveira@gmail.com>
2023-06-01 21:12:13 -0300

    Alterado em 01-06-2023

--- 05-operadores/05-exemplo-02.dart
+++ 05-operadores/05-exemplo-02.dart
@@ -2,7 +2,6 @@
 // Operadores de Atribuição
 
 void main() {
-   
   int c = 5;
   double d = 0.0;
 
@@ -10,14 +9,14 @@ void main() {
   print('c += 2 -> $c');
 
   c -= 1;
-  print('c -= 2 -> $c');
+  print('c -= 1 -> $c');
 
   c *= 3;
-  print('c *= 2 -> $c');
+  print('c *= 3 -> $c');
 
   d /= 2;
   print('d /= 2 -> $d');
 
   c %= 4;
-  print('c %= 2 -> $c');
+  print('c %= 4 -> $c');
 }
