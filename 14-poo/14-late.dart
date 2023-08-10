89a7a414078643f0370fc3f9410abad9a3a6c7e7
Ricardo Taveira <ridtaveira@gmail.com>
2023-08-10 11:40:53 -0300

    alterado em 10/08/2023

--- 14-poo/14-late.dart
+++ 14-poo/14-late.dart
@@ -8,10 +8,10 @@ class Pessoa {
 
   // Metodo Construtor
   // Inicializando as variáveis após a criação do objeto
-  Pessoa() {
-    nome = "João";
-    idade = 30;
-  }
+  // Pessoa() {
+  //  nome = "João";
+  //  idade = 30;
+ // }
 
   // Construtor Padrão não recebe nenhum parametro
   //
@@ -19,17 +19,17 @@ class Pessoa {
   // Constutor com parametros com o mesmo nome dos atributos
   // Usar o objeto atual com a palavra chave this
 
-  Pessoa(String nome, int idade) {
-    this.nome = nome;
-    this.idade = idade;
-  }
+   Pessoa(String nome, int idade) {
+     this.nome = nome;
+     this.idade = idade;
+   }
 
   // Um metodo construtor que recebe parametros e modifica
   // diretamente os atributos do objeto criado
   // Pessoa(this.nome, this.idade);
 
   //Constrtutores Nomeados
-  // Recebe 
+  // Recebe
 
   void imprimirDados() {
     print("Nome: $nome, idade: $idade");
@@ -37,8 +37,10 @@ class Pessoa {
 }
 
 void main() {
+  // Pessoa pessoa = Pessoa();
   Pessoa pessoa1 = Pessoa("Jose", 30);
   Pessoa pessoa2 = Pessoa("Maria", 24);
-  pessoa1.imprimirDados();
-  pessoa2.imprimirDados();
+  // pessoa.imprimirDados();
+    pessoa1.imprimirDados();
+    pessoa2.imprimirDados();
 }
