09cd066c888ce337bd05c39921e213e95ac43cb9
Ricardo Taveira <ridtaveira@gmail.com>
2023-08-10 19:31:11 -0300

    Alterado em 10/08/2023

--- 14-poo/14-agregacao.dart
+++ 14-poo/14-agregacao.dart
@@ -27,3 +27,15 @@ class EquipeProjeto {
     _funcionarios = funcionarios;
   }
 }
+
+void main() {
+  // 1. Criar varios objetos Dependentes
+  // 2. Criar varios objetos Funcionario
+  // 3. Associar os Dependentes criados aos respectivos
+  //    funcionarios
+  // 4. Criar uma lista de Funcionarios
+  // 5. criar um objeto Equipe Projeto chamando o metodo
+  //    contrutor que da nome ao projeto e insere uma
+  //    coleção de funcionario
+  // 6. Printar o objeto Equipe Projeto.
+}
